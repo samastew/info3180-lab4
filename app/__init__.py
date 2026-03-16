6b1d048ef16619ceb414bce054dcee5ba14a2aee
samastew <stewartsamara24@gmail.com>
2026-03-16 13:59:41 -0500

    Set u[ PostgreSQL connection with user profile

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from flask_migrate import Migrate
+
 # import flask migrate here
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+
 # Instantiate Flask-Migrate library here
+migrate = Migrate(app, db)
 
 # Flask-Login login manager
 login_manager = LoginManager()
